b8e6f68ee65d3e5640127ac3439a00ec392056df
tingting-kang <2235061002@st.usst.edu.cn>
2024-12-27 23:24:01 +0800

    为网页添加了样式

--- NewsWeb/src/main/java/com/example/newsweb/dao/DAO.java
+++ NewsWeb/src/main/java/com/example/newsweb/dao/DAO.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public interface DAO {
     // 提供数据库连接的具体实现
     public default Connection getConnection() throws SQLException {
-        String URL = "jdbc:mysql://localhost:3306/news?useUnicode=true&characterEncoding=UTF-8";//news改自己的数据库结构名
+        String URL = "jdbc:mysql://localhost:3306/news123?useUnicode=true&characterEncoding=UTF-8";//news改自己的数据库结构名
         String USER = "root";
-        String PASSWORD = "111111";
+        String PASSWORD = "itintin0811.";
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver"); // 注册数据库驱动，mySQL版本8.0
